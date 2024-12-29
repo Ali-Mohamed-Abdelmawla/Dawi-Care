79b03483555919ae40929c8341cf23dd361efe88
Ali-Mohamed-Abdelmawla <alizeyad407@gmail.com>
2024-12-29 18:36:29 +0200

    updated config for netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-app/',  // <-- Add this line
+  base: '/react-app/',
   server: {
     host: "127.0.0.1",
     port: 3000,
   },
   build: {
-    outDir: './public/build',  // Ensure the output directory matches Laravel's expectation
-    manifest: true,          // Generates manifest.json in the correct location
-    emptyOutDir: true,       // Clears the output directory before each build
+    outDir: 'dist',  // Changed to 'dist' for Netlify
+    manifest: true,
+    emptyOutDir: true,
   },
 })
+
+
+// to build for deployment on xampp
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/react-app/',  // <-- Add this line
+//   server: {
+//     host: "127.0.0.1",
+//     port: 3000,
+//   },
+//   build: { 
+//     outDir: './public/build',  // Ensure the output directory matches Laravel's expectation
+//     manifest: true,          // Generates manifest.json in the correct location
+//     emptyOutDir: true,       // Clears the output directory before each build
+//   },
+// })
